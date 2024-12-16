4f3de069a52084219618691ccba00d253378ded3
Yong Kang <yong.kang@kaamel.com>
2024-12-16 10:52:43 -0800

    everything until chapter 5

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -3,10 +3,8 @@ import { lusitana } from '@/app/ui/fonts';
 
 export default function AcmeLogo() {
   return (
-    <div
-      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
-    >
-      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
+    <div className={`${lusitana.className} flex flex-row items-center leading-none text-white`} >
+      <GlobeAltIcon className="h-12 w-12 rotate-[45deg]" />
       <p className="text-[44px]">Acme</p>
     </div>
   );
